a1e226ab7cb143ecdc39fbb8da698027d4f9842a
Oleksandr Prystupa <Oleksandr.Prystupa@invisible.io>
2019-11-26 17:24:17 +0200

    Updates

---
+++ precache-manifest.657f7aba54bfb49d039f70ff6ebd2a3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0547cfaf52a992f9e92ad33dad82ba03",
+    "url": "/github.io/index.html"
+  },
+  {
+    "revision": "4141e85c7a9c395fce1b",
+    "url": "/github.io/static/css/main.d43b530e.chunk.css"
+  },
+  {
+    "revision": "de87295e78ec6f4f2352",
+    "url": "/github.io/static/js/2.b68905cd.chunk.js"
+  },
+  {
+    "revision": "4141e85c7a9c395fce1b",
+    "url": "/github.io/static/js/main.1061db7d.chunk.js"
+  },
+  {
+    "revision": "0c911d4c543a2b0e4bb9",
+    "url": "/github.io/static/js/runtime-main.1d237fbe.js"
+  }
+]);
